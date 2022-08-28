748cd9686a7bf9d4bbdc7240a066f8dfc1c2b0a5
Anton Prokopev <nbb1@bk.ru>
2022-08-29 00:57:20 +0300

    calendar

--- ToDo/Modules/Main/TabBarController.swift
+++ ToDo/Modules/Main/TabBarController.swift
@@ -9,24 +9,18 @@
 import UIKit
 protocol TabBarOutput: AnyObject {}
 
-class TabBarController: UITabBarController {
-	
-	
-//	override func viewDidLoad() {
-//		super.viewDidLoad()
-//	}
-//
-	
+final class TabBarController: UITabBarController {
+	let dateNow = Date.now
 	
 	func createTabBarController(rootVC1: UIViewController, rootVC2: UIViewController, rootVC3: UIViewController) -> UITabBarController {
 		let tabBarVC = UITabBarController()
-		let xmrk = UIImage(systemName: "22.square.fill")
-		let chckmrk = UIImage(systemName: "22.square")
-		let settings = UIImage(systemName: "gear")
+		let firstPic = UIImage(systemName: "list.bullet.rectangle.fill")
+		let todayPic = UIImage(systemName: calendarDate())
+		let settingPic = UIImage(systemName: "gear")
 		
-		tabBarVC.setViewControllers([generationNavigationController(rootVC: rootVC1, title: "all", image: xmrk!, tag: 0),
-																generationNavigationController(rootVC: rootVC2, title: "today", image: chckmrk!, tag: 1),
-																 generationNavigationController(rootVC: rootVC3, title: "settings", image: settings!, tag: 2)], animated: true)
+		tabBarVC.setViewControllers([generationNavigationController(rootVC: rootVC1, title: "all tasks", image: firstPic!, tag: 0),
+																generationNavigationController(rootVC: rootVC2, title: "today", image: todayPic!, tag: 1),
+																 generationNavigationController(rootVC: rootVC3, title: "settings", image: settingPic!, tag: 2)], animated: true)
 		
 		func generationNavigationController(rootVC: UIViewController, title: String, image: UIImage, tag: Int) -> UINavigationController {
 			let navigationVC = UINavigationController(rootViewController: rootVC)
@@ -38,6 +32,15 @@ class TabBarController: UITabBarController {
 		rootVC2.loadViewIfNeeded()  //подгружает значек таб бара при запуске
 		rootVC3.loadViewIfNeeded()  //подгружает значек таб бара при запуске
 		return tabBarVC
+	}
+	
+	private func calendarDate() -> String {
+		let day = Calendar.current.dateComponents([.day], from: dateNow)
+		let componentsForm = DateComponentsFormatter()
+		let componentsStr = componentsForm.string(from: day)
+		let numDayStr = componentsStr!.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+		let date = "\(numDayStr).square.fill"
+		return date
 	}
 }
 
